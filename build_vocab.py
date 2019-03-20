af6bb927a822df8d9723cee27dee833985ac09a4
Jonatas Wehrmann <jonataswehrmann@gmail.com>
2019-03-20 11:40:35 -0300

    Improve build_vocab to meet new data_name format

--- build_vocab.py
+++ build_vocab.py
@@ -1,5 +1,5 @@
 import argparse
-from lavse.utils import create_logger
+from lavse.utils.logger import create_logger
 from lavse.data import get_loaders
 from lavse.tokenizers import WordTokenizer
 from pathlib import Path
@@ -16,16 +16,11 @@ if __name__ == '__main__':
         default=['f30k_precomp'],
     )
     parser.add_argument(
-        '--langs',
-        nargs='+',
-        default=['en'],
-    )
-    parser.add_argument(
         '--outpath',
     )
     args = parser.parse_args()
     
-    logger = create_logger(level='info')
+    logger = create_logger(level='debug')
 
     data_path = Path(args.data_path)
     outpath = Path(args.outpath)
@@ -33,13 +28,14 @@ if __name__ == '__main__':
     files = []
     tokenizer = WordTokenizer(download_tokenizer=True)
     for data_name in args.data_name:
-        for lang in args.langs:
-            files.extend([
-                data_path / data_name / f'train_caps.{lang}.txt',
-                data_path / data_name / f'dev_caps.{lang}.txt',
-            ])
+        data_name, lang = data_name.split('.')
+        files.extend([
+            data_path / data_name / f'train_caps.{lang}.txt',
+            data_path / data_name / f'dev_caps.{lang}.txt',
+        ])
     
     logger.info(f'Building vocab on {files}')
     tokenizer.fit_on_files(files)
     tokenizer.save(outpath)
+    logger.info(f'Saved to {outpath}')
     logger.info('Done.')
