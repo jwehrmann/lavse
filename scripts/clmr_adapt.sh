e069e9659e190ee7a0e10f97607bdabaca899951
Jonatas Wehrmann <jonataswehrmann@gmail.com>
2019-04-09 16:16:18 -0300

    Fix adapt script

--- scripts/clmr_adapt.sh
+++ scripts/clmr_adapt.sh
@@ -1,30 +1,30 @@
 
 # # Pair datasets
-# python train.py \
-# --data_path $DATA_PATH \
-# --train_data f30k_precomp.en \
-# --val_data f30k_precomp.en m30k_precomp.de \
-# --adapt_data m30k_precomp.en-de \
-# --profile clmr \
-# --outpath $OUT_PATH/lavse/clmr/f30k_precomp.en_m30k_precomp.de/
+python train.py \
+--data_path $DATA_PATH \
+--train_data f30k_precomp.en \
+--val_data f30k_precomp.en m30k_precomp.de \
+--adapt_data m30k_precomp.en-de \
+--profile clmr \
+--outpath $OUT_PATH/lavse/clmr/f30k_precomp.en_m30k_precomp.de/
 
 
-# python train.py \
-# --data_path $DATA_PATH \
-# --train_data f30k_precomp.en \
-# --val_data f30k_precomp.en jap_precomp.jt \
-# --adapt_data jap_precomp.en-jt \
-# --profile clmr \
-# --outpath $OUT_PATH/lavse/clmr/f30k_precomp.en_jap_precomp.jt/
+python train.py \
+--data_path $DATA_PATH \
+--train_data f30k_precomp.en \
+--val_data f30k_precomp.en jap_precomp.jt \
+--adapt_data jap_precomp.en-jt \
+--profile clmr \
+--outpath $OUT_PATH/lavse/clmr/f30k_precomp.en_jap_precomp.jt/
 
 
-# python train.py \
-# --data_path $DATA_PATH \
-# --train_data f30k_precomp.en \
-# --val_data f30k_precomp.en jap_precomp.jp \
-# --adapt_data jap_precomp.en-jp \
-# --profile clmr \
-# --outpath $OUT_PATH/lavse/clmr/f30k_precomp.en_jap_precomp.jp/
+python train.py \
+--data_path $DATA_PATH \
+--train_data f30k_precomp.en \
+--val_data f30k_precomp.en jap_precomp.jp \
+--adapt_data jap_precomp.en-jp \
+--profile clmr \
+--outpath $OUT_PATH/lavse/clmr/f30k_precomp.en_jap_precomp.jp/
 
 
 # All datasets
