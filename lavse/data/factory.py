f92cd6d49619e5111f6160c1cee0a56d1b0052f5
camilakolling <camilakolling@outlook.com>
2019-10-16 16:16:56 -0300

    Add tokenizer factory

---
+++ lavse/data/factory.py
@@ -0,0 +1,23 @@
+
+
+from . import tokenizer
+import torch.nn as nn
+
+import torch
+import math
+
+
+_tokenizers = {
+    'default': tokenizer.Tokenizer,
+    'bert': tokenizer.BertTokenizer,
+}
+
+
+def get_available_txtenc():
+    return _tokenizers.keys()
+
+
+def get_tokenizer(name, vocab_path):
+    model_class = _tokenizers[name]
+    model = model_class(vocab_path=vocab_path)
+    return model
