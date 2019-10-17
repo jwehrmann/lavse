95153b79db5951db232d0558caf994615115f32f
camilakolling <camilakolling@outlook.com>
2019-10-17 18:09:55 -0300

    Add bert tokenizer

--- lavse/data/tokenizer.py
+++ lavse/data/tokenizer.py
@@ -6,6 +6,7 @@ import torch
 from tqdm import tqdm
 
 import nltk
+import pytorch_transformers
 
 from ..utils.logger import get_logger
 from ..utils.file_utils import read_txt
@@ -179,122 +180,55 @@ class BertTokenizer(object):
     """
 
     def __init__(
-        self, vocab_path=None, char_level=False,
-        maxlen=None, download_tokenizer=False
+        self
     ):
-        # Create a vocab wrapper and add some special tokens.
-        self.char_level = char_level
-        self.maxlen = maxlen
+        self.unk = '[UNK]'
+        self.pad = '[PAD]'
 
-        vocab = Vocabulary()
-        vocab.add_word('<pad>')
-        vocab.add_word('<unk>')
-        vocab.add_word('<start>')
-        vocab.add_word('<end>')
+        self.tokenizer = pytorch_transformers.BertTokenizer.from_pretrained('bert-base-uncased')
+        self.vocab = self.tokenizer.vocab
+        self.idx2word = self.tokenizer.ids_to_tokens
+        self.word2idx = {value: key for key, value in self.idx2word.items()}
 
-        if char_level:
-            vocab.add_word(' ') # Space is allways #2
-
-        self.vocab = vocab
-
-        if download_tokenizer:
-            nltk.download('punkt')
-
-        if vocab_path is not None:
-            self.load(vocab_path)
-
-        logger.info(f'Loaded from {vocab_path}.')
-        logger.info(f'Created tokenizer with init {len(self.vocab)} tokens.')
-
-    def fit_on_files(self, txt_files):
-        logger.debug('Fit on files.')
-        for file in txt_files:
-            logger.info(f'Updating vocab with {file}')
-            sentences = read_txt(file)
-            self.fit(sentences)
-
-    def fit(self, sentences, threshold=4):
-        logger.debug(
-            f'Fit word vocab on {len(sentences)} and t={threshold}'
-        )
-        counter = Counter()
-
-        for tokens in tqdm(sentences, total=len(sentences)):
-            if not self.char_level:
-                tokens = self.split_sentence(tokens)
-            counter.update(tokens)
-
-        # Discard if the occurrence of the word is less than threshold
-        tokens = [
-            token for token, cnt in counter.items()
-            if cnt >= threshold
-        ]
-
-        # Add words to the vocabulary.
-        for token in tokens:
-            self.vocab.add_word(token)
-
-        logger.info(f'Vocab built. Tokens found {len(self.vocab)}')
-        return self.vocab
-
-    def save(self, outpath):
-        logger.debug(f'Saving vocab to {outpath}')
-
-        state = {
-            'word2idx': self.vocab.word2idx,
-            'char_level': self.char_level,
-            'max_len': self.maxlen
-        }
-
-        with open(outpath, "w") as f:
-            json.dump(state, f)
-
-        logger.info(
-            f'Vocab stored into {outpath} with {len(self.vocab)} tokens.'
-        )
-
-    def load(self, path):
-        logger.debug(f'Loading vocab from {path}')
-        with open(path) as f:
-            state = json.load(f)
-
-        vocab = Vocabulary()
-        vocab.word2idx = state['word2idx']
-        vocab.idx2word = {v: k for k, v in vocab.word2idx.items()}
-        vocab.idx = max(vocab.idx2word)
-        self.vocab = vocab
-        self.char_level = state['char_level']
-        self.maxlen = state['max_len']
-        logger.info(f'Loaded vocab containing {len(self.vocab)} tokens')
-        return self
+        logger.info('Loaded Bert Tokenizer')
 
     def split_sentence(self, sentence):
-        tokens = nltk.tokenize.word_tokenize(
+        tokens = self.tokenizer.tokenize(
             sentence.lower()
         )
         return tokens
 
     def tokens_to_int(self, tokens):
-        return [self.vocab(token) for token in tokens]
+        return [self.get_idx(token) for token in tokens]
 
     def tokenize(self, sentence):
         tokens = self.split_sentence(sentence)
-        if self.char_level:
-            tokens = ' '.join(tokens)
         tokens = (
-            [self.vocab('<start>')]
+              [self.word2idx[self.pad]]
             + self.tokens_to_int(tokens)
-            + [self.vocab('<end>')]
+            + [self.word2idx[self.pad]]
         )
         return torch.LongTensor(tokens)
 
     def decode_tokens(self, tokens):
         logger.debug(f'Decode tokens {tokens}')
-        join_char = '' if self.char_level else ' '
+        join_char = ' '
         text = join_char.join([
-            self.vocab.get_word(token) for token in tokens
+            self.get_word(token) for token in tokens
         ])
         return text
+
+    def get_word(self, idx):
+        if idx in self.idx2word:
+            return self.idx2word[idx]
+        else:
+            return self.unk
+
+    def get_idx(self, word):
+        if word in self.word2idx:
+            return self.word2idx[word]
+        else:
+            return self.word2idx[self.unk]
 
     def __len__(self):
         return len(self.vocab)
